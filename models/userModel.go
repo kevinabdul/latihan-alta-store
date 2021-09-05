87c505c629121543a0d4ac0a275661f2fc122a9e
kevinabdul <kevin.abdulfattah1@gmail.com>
2021-09-05 21:58:52 +0700

    modify User struct tag: add type on name, email, and password and not null constraint on email and password

--- models/userModel.go
+++ models/userModel.go
@@ -7,9 +7,9 @@ import (
 type User struct {
 	gorm.Model
 	Id     		uint 	`json:"-" gorm:"primaryKey`
-	Name   		string	`json:"name" form:"name"`
-	Email 		string	`gorm:"unique" json:"email" form:"email"`
-	Password 	string	`json:"password" form: "password"`
+	Name   		string	`gorm:"type:varchar(50)" json:"name" form:"name"`
+	Email 		string	`gorm:"unique;type:varchar(50);not null" json:"email" form:"email"`
+	Password 	string	`gorm:"type:varchar(30);not null" json:"password" form: "password"`
 }
 
 type UserAPI struct {
